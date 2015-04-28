1b194755e96bdd3307c222282fc45b9ca950a049
xushaomin <xushaomin@foxmail.com>
2015-04-28 15:53:01 +0800

    v0.0.3 redis序列化新增json支持

--- apple-cache-redis/src/main/java/com/appleframework/cache/redis/RedisCacheManager.java
+++ apple-cache-redis/src/main/java/com/appleframework/cache/redis/RedisCacheManager.java
@@ -5,6 +5,7 @@ import org.apache.log4j.Logger;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
+import com.alibaba.fastjson.JSON;
 import com.appleframework.cache.core.CacheException;
 import com.appleframework.cache.core.CacheManager;
 import com.appleframework.cache.redis.utils.SerializeUtility;
@@ -13,6 +14,8 @@ import com.appleframework.cache.redis.utils.SerializeUtility;
 public class RedisCacheManager implements CacheManager {
 
 	private static Logger logger = Logger.getLogger(RedisCacheManager.class);
+	
+	private int serializeType = 1; //序列化方式 1=byte[] =2json
 
 	private JedisPool jedisPool;
 
@@ -20,6 +23,10 @@ public class RedisCacheManager implements CacheManager {
 		this.jedisPool = jedisPool;
 	}
 	
+	public void setSerializeType(int serializeType) {
+		this.serializeType = serializeType;
+	}
+
 	@SuppressWarnings("deprecation")
 	public void clear() throws CacheException {
 		Jedis jedis = jedisPool.getResource();
@@ -36,8 +43,42 @@ public class RedisCacheManager implements CacheManager {
 	public Object get(String key) throws CacheException {
 		Jedis jedis = jedisPool.getResource();
 		try {
-			 byte[] value = jedis.get(key.getBytes());
-	         return SerializeUtility.unserialize(value);
+			if(serializeType == 1) {
+				byte[] value = jedis.get(key.getBytes());
+		     	return SerializeUtility.unserialize(value);
+			}
+			else if (serializeType == 2) {
+				return jedis.get(key);
+			}
+			else {
+				logger.error("serializeType is error");
+				return null;
+			}
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+			throw new CacheException(e.getMessage());
+		} finally {
+			jedisPool.returnResource(jedis);
+		}
+	}
+
+	@SuppressWarnings({ "unchecked", "deprecation" })
+	@Override
+	public <T> T get(String key, Class<T> clazz) throws CacheException {
+		Jedis jedis = jedisPool.getResource();
+		try {
+			if(serializeType == 1) {
+				byte[] value = jedis.get(key.getBytes());
+		     	return (T)SerializeUtility.unserialize(value);
+			}
+			else if (serializeType == 2) {
+				String message = jedis.get(key);
+				return JSON.parseObject(message, clazz);
+			}
+			else {
+				logger.error("serializeType is error");
+				return null;
+			}
 		} catch (Exception e) {
 			logger.error(e.getMessage());
 			throw new CacheException(e.getMessage());
@@ -50,7 +91,16 @@ public class RedisCacheManager implements CacheManager {
 	public boolean remove(String key) throws CacheException {
 		Jedis jedis = jedisPool.getResource();
 		try {
-			return jedis.del(key.getBytes())>0;
+			if(serializeType == 1) {
+				return jedis.del(key.getBytes())>0;
+			}
+			else if (serializeType == 2) {
+				return jedis.del(key)>0;
+			}
+			else {
+				logger.error("serializeType is error");
+				return false;
+			}
 		} catch (Exception e) {
 			logger.error(e.getMessage());
 		} finally {
@@ -64,8 +114,17 @@ public class RedisCacheManager implements CacheManager {
 		Jedis jedis = jedisPool.getResource();
 		if (null != obj) {
 			try {
-				String o = jedis.set(key.getBytes(), SerializeUtility.serialize(obj));
-				logger.info(o);
+				if(serializeType == 1) {
+					String o = jedis.set(key.getBytes(), SerializeUtility.serialize(obj));
+					logger.info(o);
+				}
+				else if (serializeType == 2) {
+					String o = jedis.set(key, JSON.toJSONString(obj));
+					logger.info(o);
+				}
+				else {
+					logger.error("serializeType is error");
+				}
 			} catch (Exception e) {
 				logger.error(e.getMessage());
 			} finally {
