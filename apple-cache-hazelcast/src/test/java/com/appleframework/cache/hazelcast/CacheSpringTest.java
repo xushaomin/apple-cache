d3db25e5e7a7aa2507947b2c594192fef19ec132
xushaomin <xushaomin@9zhitx.com>
2016-10-28 14:49:13 +0800

    新增isCacheEnable全局控制spring cache启用停止

---
+++ apple-cache-hazelcast/src/test/java/com/appleframework/cache/hazelcast/CacheSpringTest.java
@@ -0,0 +1,37 @@
+package com.appleframework.cache.hazelcast;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.appleframework.cache.core.CacheManager;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath*:config/apple-cache-ehcache.xml"})
+public class CacheSpringTest {
+
+	@Resource
+	private CacheManager cacheManager;
+	    
+	@Test
+	public void testAddOpinion1() {
+		try {
+			/*for (int i = 1; i < 100; i++) {
+				cacheManager.set("" + i, User.create("" + i, i));
+			}*/
+			for (int i = 1; i < 100; i++) {
+				cacheManager.remove(String.valueOf(i));
+			}
+			System.in.read();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+
+
+}
+
---
+++ apple-cache-hazelcast/src/test/java/com/appleframework/cache/hazelcast/TestService.java
@@ -0,0 +1,17 @@
+package com.appleframework.cache.hazelcast;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+@Service("testService")
+public class TestService {
+	
+	private long time = System.currentTimeMillis();
+
+	@Cacheable(value = "testd", key = "#name")
+	public String getCache(String name) {
+		System.out.println("-----service in------" + (System.currentTimeMillis() - time));
+		time = System.currentTimeMillis();
+		return "hello " + name;
+	}
+}
