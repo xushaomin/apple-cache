6fc53399508fb5fd8bde6d4b1c61034f85565eb8
xushaomin <xushaomin@9zhitx.com>
2016-11-09 11:57:25 +0800

    优化和精简代码

---
+++ apple-cache-core/src/main/java/com/appleframework/cache/core/spring/BaseSpringCacheManager.java
@@ -0,0 +1,41 @@
+package com.appleframework.cache.core.spring;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import org.springframework.cache.Cache;
+import org.springframework.cache.support.AbstractCacheManager;
+
+import com.appleframework.cache.core.config.CacheConfig;
+
+public abstract class BaseSpringCacheManager extends AbstractCacheManager {
+
+	protected ConcurrentMap<String, Cache> cacheMap = new ConcurrentHashMap<String, Cache>();
+	protected Map<String, Integer> expireMap = new HashMap<String, Integer>();
+
+	@Override
+	protected Collection<? extends Cache> loadCaches() {
+		Collection<Cache> values = cacheMap.values();
+		return values;
+	}
+
+	public void setExpireConfig(Map<String, Integer> expireConfig) {
+		this.expireMap = expireConfig;
+	}
+	
+	public void setCacheObject(Boolean isCacheObject) {
+		CacheConfig.setCacheObject(isCacheObject);
+	}
+	
+	public void setCacheEnable(Boolean isCacheEnable) {
+		CacheConfig.setCacheEnable(isCacheEnable);
+	}
+	
+	public void setCacheKeyPrefix(String cacheKeyPrefix) {
+		CacheConfig.setCacheKeyPrefix(cacheKeyPrefix);
+	}
+
+}
