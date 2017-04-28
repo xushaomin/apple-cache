4f82bd3b41ce5a973e80cdb4347013209041fc4c
xushaomin <xushaomin@foxmail.com>
2017-04-28 20:20:39 +0800

    优化代码

--- apple-cache-j2cache/apple-cache-j2cache-codis/src/main/java/com/appleframework/cache/j2cache/codis/spring2/SpringCacheManager.java
+++ apple-cache-j2cache/apple-cache-j2cache-codis/src/main/java/com/appleframework/cache/j2cache/codis/spring2/SpringCacheManager.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.cache.Cache;
+import org.springframework.util.StringUtils;
 
 import com.appleframework.cache.codis.CodisResourcePool;
 import com.appleframework.cache.core.spring.BaseSpringCacheManager;
@@ -30,14 +31,13 @@ public class SpringCacheManager extends BaseSpringCacheManager {
 				expireMap.put(name, expire);
 			}
 			String cacheType = cacheTypeMap.get(name);
-			if(null != cacheType && cacheType.equals("ehcache")) {
+			if(StringUtils.isEmpty(cacheType) || cacheType.equals("ehcache")) {
 				cache = new com.appleframework.cache.ehcache.spring.SpringCache(ehcacheManager, name, expire.intValue());
-				cacheMap.put(name, cache);
 			}
 			else {
 				cache = new com.appleframework.cache.codis.spring.SpringCache(codisResourcePool, name, expire.intValue());
-				cacheMap.put(name, cache);
 			}
+			cacheMap.put(name, cache);
 		}
 		return cache;
 	}
