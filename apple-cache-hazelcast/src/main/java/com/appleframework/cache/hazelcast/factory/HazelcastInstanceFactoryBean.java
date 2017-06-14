d63e63d875425a4713c8fc58617d1541fcc82d3c
徐少敏 <xushaomin@9zhitx.com>
2017-06-14 10:24:26 +0800

    初始提交

---
+++ apple-cache-hazelcast/src/main/java/com/appleframework/cache/hazelcast/factory/HazelcastInstanceFactoryBean.java
@@ -0,0 +1,46 @@
+package com.appleframework.cache.hazelcast.factory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.FactoryBean;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
+import com.hazelcast.config.NetworkConfig;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+
+public class HazelcastInstanceFactoryBean implements FactoryBean<HazelcastInstance> {
+
+	private List<String> memberList = new ArrayList<>();
+
+	public void setMembers(String members) {
+		String[] memberArray = members.split(",");
+		for (String member : memberArray) {
+			memberList.add(member);
+		}
+	}
+
+	@Override
+	public HazelcastInstance getObject() throws Exception {
+		Config config = new Config();
+		config.getNetworkConfig().setPortAutoIncrement(true);
+		NetworkConfig network = config.getNetworkConfig();
+		JoinConfig join = network.getJoin();
+		join.getMulticastConfig().setEnabled(false);
+		join.getTcpIpConfig().setMembers(memberList).setEnabled(true);
+		return Hazelcast.newHazelcastInstance(config);
+	}
+
+	@Override
+	public Class<HazelcastInstance> getObjectType() {
+		return HazelcastInstance.class;
+	}
+
+	@Override
+	public boolean isSingleton() {
+		return false;
+	}
+
+}
